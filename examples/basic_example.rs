5e48f0084b73e91cc4d80e3bb51cf930469d04c6
Drew Chase <dcmanproductions@gmail.com>
2025-07-04 20:01:10 -0400

    Add basic example for `AsynchronousInteractiveProcess` usage

---
+++ examples/basic_example.rs
@@ -0,0 +1,40 @@
+use aisph::AsynchronousInteractiveProcess;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let pid = AsynchronousInteractiveProcess::new("test_server/target/release/test_server.exe").start().await?;
+
+    // From another thread...
+    let reader_thread = tokio::spawn(async move {
+        let process = AsynchronousInteractiveProcess::get_process_by_pid(pid).await.expect("Process not found");
+        let mut i = 0;
+        while process.is_process_running().await {
+            // Send input to the process every 5 iterations
+            if i % 5 == 0 {
+                if let Err(e) = process.send_input("echo this").await {
+                    eprintln!("[SERVER ERROR]: {}", e);
+                }
+            }
+
+            if i > 10 {
+                if let Err(e) = process.kill().await {
+                    eprintln!("[SERVER ERROR]: {}", e);
+                } else {
+                    println!("[SERVER]: Process killed after 10 iterations.");
+                    break;
+                }
+            }
+
+            // Read output from the process
+            let line = process.receive_output().await;
+            if let Some(output) = line {
+                println!("[SERVER]: {}", output);
+                i += 1;
+            }
+        }
+    });
+
+    reader_thread.await?;
+
+    Ok(())
+}
